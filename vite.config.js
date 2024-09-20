96972c235582726d8dc9ff7484bccda22028a5a9
wxc <xichang.wu@holla.world>
2024-09-20 15:27:00 +0800

    feat: 修改路径

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/danmuku/', // 替换为你的仓库名
+  base: '/danmu/', // 替换为你的仓库名
 })
 
 
